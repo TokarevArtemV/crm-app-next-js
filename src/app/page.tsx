89786c8eca7559528c50953798bd289721e603d6
Artem Tokarev <tokarev.artem.v@gmail.com>
2024-08-22 14:45:27 +0300

    Add Dynamic Routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
-import ActiveLabel, { Status } from './components/Labels/status-label';
+import clsx from 'clsx';
+
+import AddCompanyButton from './components/add-company-button';
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl">Hello</h1>
-      <ActiveLabel status={Status.Active}>Active</ActiveLabel>
-      <ActiveLabel status={Status.NotActive}>Not Active</ActiveLabel>
-      <ActiveLabel status={Status.Pending} disabled={true}>
-        Panding
-      </ActiveLabel>
-      <ActiveLabel status={Status.Suspended}>Suspended</ActiveLabel>
+      <h1 className={clsx('text-xl')}>Home page</h1>
+      <AddCompanyButton />
     </main>
   );
 }
